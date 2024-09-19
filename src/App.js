19c63017fcaeafd52e74a7d5ca348ad9a0519368
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-09-19 15:21:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import WElcome from "./WElcome";
+
+function App() {
+  return (
+    <div className="App">
+      <WElcome></WElcome>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/WElcome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WElcome = () => {
+  return (
+    <div>
+      <h1>Hello & Welcome</h1>
+    </div>
+  );
+};
+
+export default WElcome;
