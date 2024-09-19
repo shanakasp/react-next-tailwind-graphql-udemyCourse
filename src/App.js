c56d66b07700980a3bcfd4e5d765cd6117377600
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-09-19 21:19:46 +0530

    modified App.js in src

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import Names from "./Names";
 import WElcome from "./WElcome";
 
 function App() {
+  const buttonClick = () => {
+    alert("Hello World");
+  };
+
   return (
     <div className="App">
       <WElcome></WElcome>
       <Names></Names>
       <Greetings></Greetings>
+      <button onClick={buttonClick}>Click me</button>
     </div>
   );
 }
