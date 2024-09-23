d7760df4be1eb6e01a346a9418e91b47d74317ce
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-09-23 12:33:40 +0530

    changed 6 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import "./App.css";
-import Card from "./Card";
-import Greetings from "./Greetings";
-import LoginForm from "./LoginForm";
-import MyForm from "./MyForm";
-import Names from "./Names";
-import WElcome from "./WElcome";
 
 function App() {
   const buttonClick = () => {
@@ -13,18 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <WElcome></WElcome>
-      <Names></Names>
-      <Greetings username=" user ABCD"></Greetings>
-      <button onClick={buttonClick}>Click me</button>
-      <MyForm></MyForm>
-      <Card
-        name=" hello im shanaka"
-        bio=" Hello HEllo this is bio"
-        image="https://www.supercars.net/blog/wp-content/uploads/2022/09/Best-New-Sports-and-Performance-Cars-2022_Chevrolet_Corvette.jpg"
-      ></Card>
-
-      <LoginForm></LoginForm>
+      <h1>Task Manager</h1>
     </div>
   );
 }
