afeff3af977ff8872d9566d54dc7d99235c3b1ff
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-09-23 09:41:47 +0530

    changed 3 files

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,10 +2,24 @@ import React from "react";
 
 const Card = (props) => {
   return (
-    <div>
-      <img src={props.image}></img>
-      <h2>{props.name}</h2>
-      <h3>{props.bio}</h3>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        margin: "20px",
+        backgroundColor: "#D3F3EF",
+        borderRadius: "10px",
+      }}
+    >
+      <img
+        style={{ width: "50%", margin: "20px", borderRadius: "10px" }}
+        src={props.image}
+      ></img>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        {" "}
+        <h2>{props.name}</h2>
+        <h3>{props.bio}</h3>
+      </div>
     </div>
   );
 };
