b1cc23825f53ad967c183521d5121a8b7f5da330
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-09-28 19:46:47 +0530

    modified App.js in src

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Counter from "./Counter";
 import CounterReducer from "./CounterReducer";
@@ -8,6 +8,14 @@ import { userContext } from "./UserContext";
 
 function App() {
   const user = { name: "john", age: "30" };
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  });
 
   return (
     <div className="App">
@@ -19,6 +27,7 @@ function App() {
       <Counter />
       <CounterReducer></CounterReducer>
       <UseRef />
+      {data && data.map((item) => <p key={item.id}>{item.title}</p>)}
     </div>
   );
 }
